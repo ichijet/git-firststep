18d276efe45ee1759c8e07a3fd910fe6f785e901
Seiichiro Amai <sweetjetsetter@gmail.com>
2020-05-04 15:55:37 +0900

    update

--- yamanote.js
+++ yamanote.js
@@ -20,6 +20,8 @@ async function getRequest() {
     let unko = html.match(/id="mdServiceStatus">(.*?)<\/div>/)[1];
     unko = unko.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''); //整形2: タグを削除
     console.log(unko);
+
+    await updateData({msg: unko}); //データ更新関数を実行
   } catch (error) {
     console.error(error);
   }
