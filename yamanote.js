c53f3565705d6aeaa35157d05fff2c6052c2d450
Seiichiro Amai <sweetjetsetter@gmail.com>
2020-05-04 15:52:25 +0900

    update

--- yamanote.js
+++ yamanote.js
@@ -2,6 +2,13 @@
 'use strcit';
 
 const axios = require('axios');
+const fs = require('fs'); //注: npm i 不要
+
+//データ更新関数
+async function updateData(newData){
+    const PATH = './docs/data.json';
+    fs.writeFileSync(PATH, JSON.stringify(newData));
+}
 
 // 実際にデータを取得する getRequest 関数
 async function getRequest() {
